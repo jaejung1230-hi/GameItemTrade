925febea1987f165bd9fbece2c3c6dd2dc2465ee
JaeJung <jaejung1230-hi>
2021-07-14 15:23:20 +0900

    20210714 eighth commit
    UserInfoActivity에서 간단한 개인정보 외에도 해당 유저가 등록한 아이템과 해당 유저에 대한 신고를 확인 할 수 있도록 하였으며 해당 내역은 RecycleView에 저장되도록 함

--- app/src/main/java/com/example/gameitemtrade/UserInfoActivity.kt
+++ app/src/main/java/com/example/gameitemtrade/UserInfoActivity.kt
@@ -1,14 +1,25 @@
 package com.example.gameitemtrade
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gameitemtrade.Data.ItemInfomation
+import com.example.gameitemtrade.Data.ReportObject
+import com.example.gameitemtrade.Main_Fragment.ItemListAdapter
+import com.example.gameitemtrade.Main_Fragment.ReportListAdapter
+import com.example.gameitemtrade.Tasks.GetReportTask
+import com.example.gameitemtrade.Tasks.SelectItemTask
 import com.example.gameitemtrade.Tasks.UserInfoTask
 import com.squareup.picasso.Picasso
 import org.json.JSONArray
 
 class UserInfoActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
@@ -19,6 +30,10 @@ class UserInfoActivity : AppCompatActivity() {
         val iv_selectUser_pic = findViewById(R.id.iv_selectUser_pic) as ImageView
         val tv_selectUser_name = findViewById(R.id.tv_selectUser_name) as TextView
         val tv_selectUser_point = findViewById(R.id.tv_selectUser_point) as TextView
+        val btn_users_item = findViewById(R.id.btn_users_item) as TextView
+        val btn_users_report = findViewById(R.id.btn_users_report) as TextView
+        val recyclerview_users = findViewById(R.id.recyclerview_users) as RecyclerView
+
         val loginTask = UserInfoTask()
         val result = loginTask.execute(select_user).get()
 
@@ -28,5 +43,46 @@ class UserInfoActivity : AppCompatActivity() {
         tv_selectUser_name.text = User.getString("nickName")
         tv_selectUser_point.text = User.getInt("attitude").toString() + "점의 유저입니다!!"
 
+        btn_users_item.setOnClickListener {
+            btn_users_item.setBackgroundResource(R.drawable.custom_login_button)
+            btn_users_item.setTextColor(R.color.main_color)
+            btn_users_report.setBackgroundResource(R.drawable.custom_solid_buttom)
+            btn_users_report.setTextColor(R.color.white)
+            val task = SelectItemTask()
+            var result = task.execute(User.getString("userID"),"Find_MyItem").get()
+
+            val itemListArray = JSONArray(result)
+            val itemInfoList: ArrayList<ItemInfomation> = arrayListOf<ItemInfomation>()
+            if (!result.equals("[]")) {
+                for (i in 0..(itemListArray.length() - 1)) {
+                    itemInfoList.add(ItemInfomation(itemListArray.getJSONObject(i)))
+                }
+            }
+            val adapter = ItemListAdapter(itemInfoList)
+            recyclerview_users.setLayoutManager(LinearLayoutManager(this))
+            recyclerview_users.setAdapter(adapter)
+
+        }
+
+        btn_users_report.setOnClickListener {
+            btn_users_report.setBackgroundResource(R.drawable.custom_login_button)
+            btn_users_report.setTextColor(R.color.main_color)
+            btn_users_item.setBackgroundResource(R.drawable.custom_solid_buttom)
+            btn_users_item.setTextColor(R.color.white)
+
+            val task = GetReportTask()
+            var result = task.execute(User.getString("userID"),"Get_report_user").get()
+
+            val reportListArray = JSONArray(result)
+            val reportList: ArrayList<ReportObject> = arrayListOf<ReportObject>()
+            if (!result.equals("[]")) {
+                for (i in 0..(reportListArray.length() - 1)) {
+                    reportList.add(ReportObject(reportListArray.getJSONObject(i)))
+                }
+            }
+            val adapter = ReportListAdapter(reportList)
+            recyclerview_users.setLayoutManager(LinearLayoutManager(this))
+            recyclerview_users.setAdapter(adapter)
+        }
     }
 }
